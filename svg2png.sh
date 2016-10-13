65401b2c9d0db67273c18320ebf03f2825f7264d
izumin5210 <masayuki@izumin.info>
2016-10-13 17:52:21 +0900

    Fix image size

--- svg2png.sh
+++ svg2png.sh
@@ -11,7 +11,7 @@ for svg in $src/*/*-original.svg
 do
     png=$dist/$(echo $svg | awk -F/ '{ print $NF }' | sed -e s/-original\.svg/\.png/)
     name=$(echo $png | awk -F/ '{ print $NF }'g | awk -F. '{ print $1 }')
-    inkscape --without-gui --export-width=256 --file=$svg --export-png=$png
+    inkscape --without-gui --export-width=128 --file=$svg --export-png=$png
     echo "  - name: $name" >> $yaml
     echo "    src: https://raw.githubusercontent.com/izumin5210/emojipack-for-devicon/master$png" >> $yaml
 done
